886c87c0e9d7e9dc4c92b4341c956c57ab126271
ABHISHEK RAMAWAT <90638230+abhishake99@users.noreply.github.com>
2023-04-13 19:18:43 +0530

    Update index.py

--- index.py
+++ index.py
@@ -17,33 +17,25 @@ en = spacy.load('en_core_web_sm')
 stopwords = en.Defaults.stop_words
 punc=string.punctuation
 lemmatizer = WordNetLemmatizer()
+def lowern(text):
+    text=text.lower()
+    return text
 def punc_remover(text):
     for i in text:
-        i[0]=str(i[0])
-        for j in i[0]:
-            if j in punc:
-                i[0]=i[0].replace(j,'')
-    return text
-
-def lowercase_list(lst):
-    
-    for i in  lst:
-        i[0]=str(i[0])
-        i[0]=np.char.lower(i[0])
-    return lst
+        if i in punc:
+            text=text.replace(i,'')
+    return str(text)
 
 def stopwords_remover(text):
-    for i in text:
-        i[0]=str(i[0])
-        split_text=i[0].split()
-        new_text=[]
-        for j in split_text:
-            if j not in stopwords:
-                new_text.append(j)
-        i[0]= " ".join(new_text)
-    return text        
+    text=str(text)
+    split_text=text.split()
+    new_text=[]
+    for i in split_text:
+        if i not in stopwords:
+            new_text.append(i)
+    return " ".join(new_text)        
 
-def remove_emojis(data):
+def remove_emojis(text):
     emoj = re.compile("["
         u"\U0001F600-\U0001F64F"  # emoticons
         u"\U0001F300-\U0001F5FF"  # symbols & pictographs
@@ -63,17 +55,17 @@ def remove_emojis(data):
         u"\u231a"
         u"\ufe0f"  # dingbats
         u"\u3030"
-                      "]+", re.UNICODE)
-    for i in data:
-        i[0]=str(i[0])
-        i[0]=re.sub(emoj, '', i[0])
-    return data            
+                    "]+", re.UNICODE)
+    return re.sub(emoj, '', text)            
 
-def cleanhtml(raw_html):
-   for i in raw_html:
-        i[0]=str(i[0])
-        i[0] = re.sub(CLEANR, '', i[0])
-   return raw_html
+# def spell_correct(text):
+#     text=str(text)
+#     textblb=TextBlob(text)
+#     return textblb.correct().string 
+
+def cleanhtml(text):
+    cleantext = re.sub(CLEANR, '', text)
+    return cleantext
 
 def get_simple_pos(tag):
     if tag.startswith('J'):
@@ -88,37 +80,41 @@ def get_simple_pos(tag):
         return wordnet.NOUN
 
 def lemmatizerrr(text):
-    for i in text:
-        i[0]=str(i[0])
-        split_text=i[0].split()
-        new_text=[]
-        for j in split_text:
-            tag=pos_tag([j])
-            lemmatizer.lemmatize(j,pos=get_simple_pos(tag[0][1]))
-        i[0]= " ".join(split_text)
+    text=str(text)
+    split_text=text.split()
+    new_text=[]
+    for i in split_text:
+        tag=pos_tag([i])
+        lemmatizer.lemmatize(i,pos=get_simple_pos(tag[0][1]))
+    return " ".join(split_text) 
+
+def othercleaning(text):
+    text = re.sub('((www\.[^\s]+)|(https?://[^\s]+))','',text) #remove links
+    text = re.sub('@[^\s]+','',text) #remove usernames
+    text = re.sub('[\s]+', ' ', text)
     return text 
+def masterfunction(text):
+    text=lowern(text)
+    text=punc_remover(text)
+    text=stopwords_remover(text)
+    text=remove_emojis(text)
+    text=cleanhtml(text)
+    text=lemmatizerrr(text)
+    text=othercleaning(text)
+    return text              
 
-def othercleaning(tweet):
-    for i in tweet:
-        i[0]=str(i[0])
-        i[0] = re.sub('((www\.[^\s]+)|(https?://[^\s]+))','',i[0]) #remove links
-        i[0] = re.sub('@[^\s]+','',i[0]) #remove usernames
-        i[0] = re.sub('[\s]+', ' ', i[0])
-    return tweet       
-
-with open('pipeline1.pkl', 'rb') as f1:
-    pipeline1 = pickle.load(f1)
 with open('pipeline2.pkl', 'rb') as f2:
     pipeline2 = pickle.load(f2)
     
 st.title("Sentiment Predictor")
 input_sms = st.text_area("Enter the message")
 if st.button('Predict'):
-    trans=pipeline1.transform(np.array(input_sms).reshape(-1,1))
-    pre=pipeline2.predict(trans[0])
+    trans=masterfunction(input_sms)
+    pre=pipeline2.predict([trans])
     if pre[0] == 'positive':
         st.header("Positive")
     elif pre[0] == 'negative':
         st.header("Negative")
     else:
         st.header("Neutral")
+
