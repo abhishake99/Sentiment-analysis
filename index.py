e4a711de7b179c25fa63f3f8668cd5e2c0fe2fc6
ABHISHEK RAMAWAT <90638230+abhishake99@users.noreply.github.com>
2023-06-09 19:50:10 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,128 +1,52 @@
 import pickle
-import re
-import json
-import spacy
-import string
-import nltk
-import numpy as np
-from nltk.stem import WordNetLemmatizer
-from nltk import pos_tag
-from nltk.corpus import wordnet
-from cleantext import clean
-from sklearn.pipeline import Pipeline, make_pipeline
+from keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.text import Tokenizer
 import streamlit as st
-nltk.download('punkt')
-nltk.download('stopwords')
-nltk.download('corpus')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('wordnet')
-nltk.download('omw-1.4')
 
-CLEANR = re.compile('<.*?>') 
-en = spacy.load('en_core_web_sm')
-stopwords = en.Defaults.stop_words
-punc=string.punctuation
-lemmatizer = WordNetLemmatizer()
-def lowern(text):
-    text=text.lower()
-    return text
-def punc_remover(text):
-    for i in text:
-        if i in punc:
-            text=text.replace(i,'')
-    return str(text)
+model = load_model('best_model.h5')
+with open('tokenizer.pickle', 'rb') as handle:
+    tokenizer = pickle.load(handle)
 
-def stopwords_remover(text):
-    text=str(text)
-    split_text=text.split()
-    new_text=[]
-    for i in split_text:
-        if i not in stopwords:
-            new_text.append(i)
-    return " ".join(new_text)        
+def predict_class(text):
+    '''Function to predict sentiment class of the passed text'''
+    
+    sentiment_classes = ['Negative', 'Neutral', 'Positive']
+    max_len=50
+    
+    # Transforms text to a sequence of integers using a tokenizer object
+    xt = tokenizer.texts_to_sequences(text)
+    # Pad sequences to the same length
+    xt = pad_sequences(xt, padding='post', maxlen=max_len)
+    # Do the prediction using the loaded model
+    pre=model.predict(xt)
+    #print(pre)
+    yt = pre.argmax(axis=1)
+    return sentiment_classes[yt[0]],round(pre[0][yt[0]]*100,4)
 
-def remove_emojis(text):
-    emoj = re.compile("["
-        u"\U0001F600-\U0001F64F"  # emoticons
-        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-        u"\U0001F680-\U0001F6FF"  # transport & map symbols
-        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-        u"\U00002500-\U00002BEF"  # chinese char
-        u"\U00002702-\U000027B0"
-        u"\U00002702-\U000027B0"
-        u"\U000024C2-\U0001F251"
-        u"\U0001f926-\U0001f937"
-        u"\U00010000-\U0010ffff"
-        u"\u2640-\u2642" 
-        u"\u2600-\u2B55"
-        u"\u200d"
-        u"\u23cf"
-        u"\u23e9"
-        u"\u231a"
-        u"\ufe0f"  # dingbats
-        u"\u3030"
-                    "]+", re.UNICODE)
-    return re.sub(emoj, '', text)            
-
-# def spell_correct(text):
-#     text=str(text)
-#     textblb=TextBlob(text)
-#     return textblb.correct().string 
-
-def cleanhtml(text):
-    cleantext = re.sub(CLEANR, '', text)
-    return cleantext
-
-def get_simple_pos(treebank_tag):
-    if treebank_tag.startswith('J'):
-        return wordnet.ADJ
-    elif treebank_tag.startswith('V'):
-        return wordnet.VERB
-    elif treebank_tag.startswith('N'):
-        return wordnet.NOUN
-    elif treebank_tag.startswith('R'):
-        return wordnet.ADV
-    else:
-        return wordnet.NOUN
-
-def lemmatizerrr(sentence):
-    nltk_tagged = nltk.pos_tag(nltk.word_tokenize(sentence))  
-    wordnet_tagged = map(lambda x: (x[0], get_simple_pos(x[1])), nltk_tagged)
-    lemmatized_sentence = []
-    for word, tag in wordnet_tagged:
-        if tag is None:
-            lemmatized_sentence.append(word)
-        else:        
-            lemmatized_sentence.append(lemmatizer.lemmatize(word, tag))
-    return " ".join(lemmatized_sentence)
-
-def othercleaning(text):
-    text = re.sub('((www\.[^\s]+)|(https?://[^\s]+))','',text) #remove links
-    text = re.sub('@[^\s]+','',text) #remove usernames
-    text = re.sub('[\s]+', ' ', text)
-    return text 
-def masterfunction(text):
-    text=lowern(text)
-    text=punc_remover(text)
-    text=stopwords_remover(text)
-    text=remove_emojis(text)
-    text=cleanhtml(text)
-    text=lemmatizerrr(text)
-    text=othercleaning(text)
-    return text              
-
-with open('pipeline2upn.pkl', 'rb') as f2:
-    pipeline2 = pickle.load(f2)
     
 st.title("Sentiment Predictor")
-input_sms = st.text_area("Enter the message")
-if st.button('Predict'):
-    trans=masterfunction(input_sms)
-    pre=pipeline2.predict([trans])
-    if pre[0] == 'positive':
-        st.header("Positive")
-    elif pre[0] == 'negative':
-        st.header("Negative")
-    else:
-        st.header("Neutral")
-
+st.markdown("This sentiment app is trained on Bidirectional LSTM neural network with almost 1 Lakh texts. This app works by tokenzing the entered text into padded integer sequence and then send to model for prediction.")
+#st.subheader("What do want to do? Click on your desired task button")
+task1="Predict sentiment for some random text"
+task2="Work with twitter api"
+desire=st.radio("Choose your desired task please:-",(task1,task2))
+if desire==task1:
+    input_sms = st.text_area("Enter the message")
+    if st.button('Predict'):
+        pre,prob=predict_class([input_sms])
+        if pre == 'Positive':
+            st.header("Positive")
+            st.subheader("Percentage of Positivity : {prob}".format(prob=prob))
+        elif pre == 'Negative':
+            st.header("Negative")
+            st.subheader("Percentage of Negativity : {prob}".format(prob=prob))
+        else:
+            st.header("Neutral")
+            st.subheader("Percentage of Neutrality : {prob}".format(prob=prob))
+if desire==task2:
+    st.header("Currently in development stage")
+    # consumerKey = st.text_input("consumer_key")
+    # consumerSecret = st.text_input("consumer_key_secret")
+    # accessToken = st.text_input("access_token")
+    # accessTokenSecret = st.text_input("access_token_secret")
